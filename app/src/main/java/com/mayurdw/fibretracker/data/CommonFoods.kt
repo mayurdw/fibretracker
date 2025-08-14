f225136a65df4e564d38cdde39cb63c70445792a
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-08-14 19:01:54 +1200

    Basic Screen Elements

--- app/src/main/java/com/mayurdw/fibretracker/data/CommonFoods.kt
+++ app/src/main/java/com/mayurdw/fibretracker/data/CommonFoods.kt
@@ -2,14 +2,14 @@ package com.mayurdw.fibretracker.data
 
 import com.mayurdw.fibretracker.model.FoodItem
 
-enum class CommonFoods(val foodItem: FoodItem) {
-    OATS(FoodItem("Oats", 50, 120)),
-    CHIA(FoodItem("Chia", 10, 344)),
-    GLUTEN_FREE_BREAD(FoodItem("GF Bread", 70, 154)),
-    RICE_CAKE(FoodItem("Rice Cake", 35, 35)),
-    BANANA(FoodItem("Banana", 120, 26)),
-    POTATO(FoodItem("Potato", 110, 22)),
-    BEAN(FoodItem("Beans", 50, 34)),
-    CARROT(FoodItem("Carrot", 50, 32)),
-    PSYLLIUM_HUSK(FoodItem("Psyllium Husk", 2, 800))
-}
+val CommonFoods = listOf(
+    FoodItem("Oats", 50, 120),
+    FoodItem("Chia", 10, 344),
+    FoodItem("GF Bread", 70, 154),
+    FoodItem("Rice Cake", 35, 35),
+    FoodItem("Banana", 120, 26),
+    FoodItem("Potato", 110, 22),
+    FoodItem("Beans", 50, 34),
+    FoodItem("Carrot", 50, 32),
+    FoodItem("Psyllium Husk", 2, 800)
+)
--- app/src/main/java/com/mayurdw/fibretracker/ui/components/AddEntryView.kt
+++ app/src/main/java/com/mayurdw/fibretracker/ui/components/AddEntryView.kt
@@ -1,16 +1,56 @@
 package com.mayurdw.fibretracker.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mayurdw.fibretracker.data.CommonFoods
 import com.mayurdw.fibretracker.ui.theme.FibreTrackerTheme
 
 @Composable
 fun AddEntryView(modifier: Modifier = Modifier) {
+    val foodItem = remember { mutableStateOf(CommonFoods[0].foodName) }
+    val foodQuantity = remember { mutableStateOf(CommonFoods[0].foodAmountInGrams.toString()) }
 
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+        ) {
+        TextField(
+            value = foodItem.value,
+            onValueChange = { newValue: String -> foodItem.value = newValue },
+            label = { Text("Food Item") },
+            placeholder = { Text("Chia") },
+        )
+        TextField(
+            value = foodQuantity.value,
+            onValueChange = { newValue: String -> foodQuantity.value = newValue },
+            label = { Text("Quantity in Grams") },
+            placeholder = { Text("Chia") },
+        )
+
+        Text(modifier = modifier, text = "Fibre Per MilliGram = ${CommonFoods[0].fiberPerMilliGrams}")
+
+        Text(modifier = modifier, text = "Fiber Consumed = ${CommonFoods[0].totalFiberInFood / 1000}")
+
+        Button(onClick = { }, content = { Text("Submit") } )
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun AddEntryPreview() {
     FibreTrackerTheme {
