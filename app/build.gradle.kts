4e7ba81ba91ef85fe549e8ff74f7a3b8e24e594a
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-08-20 19:44:02 +1200

    Added ViewModel dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation)
+    implementation(libs.androidx.lifecycle.viewModel)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
