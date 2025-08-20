2e1f8ae5263108841da8c3e2fcf34171b5c7ee28
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-08-20 19:08:29 +1200

    Setup Basic Navigation (Untested)

--- app/src/main/java/com/mayurdw/fibretracker/MainActivity.kt
+++ app/src/main/java/com/mayurdw/fibretracker/MainActivity.kt
@@ -8,7 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mayurdw.fibretracker.data.CommonFoods
+import com.mayurdw.fibretracker.ui.components.AddEntryView
+import com.mayurdw.fibretracker.ui.components.AddFoodItemList
+import com.mayurdw.fibretracker.ui.destinations.AddAmountItem
+import com.mayurdw.fibretracker.ui.destinations.AddFoodItem
+import com.mayurdw.fibretracker.ui.destinations.Home
 import com.mayurdw.fibretracker.ui.theme.FibreTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,11 +26,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FibreTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text("Hello World", modifier = Modifier.padding(innerPadding))
-                }
+            val navController = rememberNavController()
+
+            NavHost(navController, startDestination = Home) {
+                composable<Home> { HomeScreen() }
+                composable<AddFoodItem> { AddFoodItemList(foodItems = emptyList()) { } }
+                composable<AddAmountItem> { AddEntryView(selectedFoodItem = CommonFoods[0]) }
             }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    FibreTrackerTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Text("Hello World", modifier = Modifier.padding(innerPadding))
         }
     }
 }
---
+++ app/src/main/java/com/mayurdw/fibretracker/ui/destinations/Destinations.kt
@@ -0,0 +1,12 @@
+package com.mayurdw.fibretracker.ui.destinations
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+object AddFoodItem
+
+@Serializable
+object AddAmountItem
