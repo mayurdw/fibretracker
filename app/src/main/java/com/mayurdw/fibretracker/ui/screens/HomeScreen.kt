727d189777e5f4255f42d8bc93614d022b08ad9e
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-10-04 10:58:24 +1300

    Starting with layout

--- app/src/main/java/com/mayurdw/fibretracker/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mayurdw/fibretracker/ui/screens/HomeScreen.kt
@@ -1,27 +1,32 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.mayurdw.fibretracker.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mayurdw.fibretracker.R
 import com.mayurdw.fibretracker.ui.theme.FibreTrackerTheme
+import com.mayurdw.fibretracker.viewmodels.DateData
 import com.mayurdw.fibretracker.viewmodels.FoodListItem
+import com.mayurdw.fibretracker.viewmodels.HomeData
 import com.mayurdw.fibretracker.viewmodels.HomeScreenViewModel
 import com.mayurdw.fibretracker.viewmodels.HomeState
-import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -35,7 +40,7 @@ fun HomeScreen(
     when (homeState) {
         is HomeState.Success -> {
             val homeData = homeState as HomeState.Success
-            FoodItems(foodItems = homeData.data.dateData.foodItems)
+            HomeScreenLayout(modifier = modifier, homeData.data)
         }
 
         else -> {
@@ -43,6 +48,28 @@ fun HomeScreen(
         }
     }
 }
+
+
+@Composable
+fun HomeScreenLayout(modifier: Modifier, homeData: HomeData) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(modifier = modifier.fillMaxWidth().padding(top = 16.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.Bottom) {
+            IconButton(onClick = {}, enabled = homeData.hasPrevious) {
+                Icon(painterResource(R.drawable.previous), contentDescription = null)
+            }
+
+            Text(modifier = modifier, text = homeData.dateData.formattedDate)
+
+            IconButton(onClick = {}, enabled = homeData.hasNext) {
+                Icon(painterResource(R.drawable.next), contentDescription = null)
+            }
+        }
+
+
+
+    }
+}
+
 
 @Composable
 fun FoodItems(modifier: Modifier = Modifier, foodItems: List<FoodListItem>) {
@@ -61,14 +88,23 @@ fun FoodItems(modifier: Modifier = Modifier, foodItems: List<FoodListItem>) {
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun PreviewFoodItems() {
+private fun PreviewHomeScreenLayout() {
     FibreTrackerTheme {
-        FoodItems(
-            foodItems = listOf(
-                FoodListItem(id = 1, foodQuantity = "34.9", foodName = "Potato"),
-                FoodListItem(id = 2, foodQuantity = "15.23", foodName = "Chia")
+        HomeScreenLayout(
+            Modifier, HomeData(
+                hasPrevious = false,
+                hasNext = true,
+                dateData = DateData(
+                    "29/5/25",
+                    fibreOfTheDay = "29.4",
+                    foodItems = listOf(
+                        FoodListItem(id = 1, foodQuantity = "34.9", foodName = "Potato"),
+                        FoodListItem(id = 2, foodQuantity = "15.23", foodName = "Chia")
+                    )
+                )
             )
         )
     }
