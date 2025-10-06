ff89342f0c2078c107f0b2d70dd1a2c5c8811d7d
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-10-07 06:59:04 +1300

    Got Food List working

--- app/src/main/java/com/mayurdw/fibretracker/ui/screens/AddFoodItemList.kt
+++ app/src/main/java/com/mayurdw/fibretracker/ui/screens/AddFoodItemList.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +29,7 @@ fun AddFoodItemLayout(
     viewModel: AddFoodEntryViewModel = viewModel(),
     onItemSelect: () -> Unit
 ) {
-    val entries = viewModel.entryState.collectAsState()
+    val entries by viewModel.entryState.collectAsState()
     viewModel.loadData()
 
     when (entries) {
