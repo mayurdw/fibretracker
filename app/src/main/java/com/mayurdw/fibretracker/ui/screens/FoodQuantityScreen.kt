a76a1bd52be81939a718c29457464802febdf012
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-11-14 19:03:31 +1300

    Got Keyboard Type Working
    Issue:
    - The number on home page is wrong

--- app/src/main/java/com/mayurdw/fibretracker/ui/screens/FoodQuantityScreen.kt
+++ app/src/main/java/com/mayurdw/fibretracker/ui/screens/FoodQuantityScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -95,6 +97,9 @@ private fun FoodQuantityScreenContent(
             state = foodQuantity,
             label = { Text("Quantity in Grams") },
             placeholder = { Text("${selectedFoodItem.singleServingSizeInGm}") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            )
         )
 
         Text(
