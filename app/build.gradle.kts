db97112fa5799bc53e957a6570b255fc27765fbd
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-01-05 18:46:25 +1300

    v0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mayurdw.fibretracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
