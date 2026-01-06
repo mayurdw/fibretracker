25a84c3fc653594268e51a6133ca1b20b3506098
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-01-06 18:00:47 +1300

    Fixed Build Output Path & version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.mayurdw.fibretracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
